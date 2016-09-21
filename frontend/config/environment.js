6ada9bc6161bf2efd31d36e4c1ab01e3c4ef6906
Tyler James Newman <tyler.newman@runtriz.com>
2016-09-21 14:00:25 -0700

    add admin route

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function (environment) {
   var ENV = {
-    modulePrefix: 'guest-list-frontend',
+    modulePrefix: 'frontend',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
