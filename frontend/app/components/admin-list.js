2a36b2ab1e80500fac29ea31fb2149d96b1cf8da
Tyler James Newman <tyler.newman@runtriz.com>
2016-09-21 16:35:01 -0700

    add table styling and case insentive matching

--- frontend/app/components/admin-list.js
+++ frontend/app/components/admin-list.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  filterText: '',
+
+  dataFromPromise: Ember.computed('filterText', function () {
+    let users = this.get('users');
+    let filterText = this.get('filterText').toLowerCase();
+    if (filterText === '') {
+      return users;
+    }
+
+    return users.filter(function (user) {
+      return user.get('name').toLowerCase().match(filterText);
+    });
+  }),
+
+  actions: {
+      updateFilter(str) {
+        this.set('filterText', str);
+      },
+    },
 });
